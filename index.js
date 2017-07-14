5e5610d712b7e1ce16993bdd5a5fea9692f3ba23
Eryk Napierala <eryk.napierala@allegrogroup.com>
2017-07-14 12:43:35 +0200

    transform to remove-at-rules plugin

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 var postcss = require('postcss');
 
-module.exports = postcss.plugin('remove-rules', function (opts) {
-    opts = opts || {};
-    var rulesToRemove = opts.rulesToRemove;
-
+module.exports = postcss.plugin('remove-rules', function (rulesToRemove) {
     return function (css) {
-        if (rulesToRemove) {
-            css.walkRules(function (rule) {
-                var excludes = rulesToRemove[rule.selector];
-                if (excludes) {
-                    // One or more rules should be removed
-                    if (excludes === '*') {
-                        // Remove this entire rule
-                        rule.remove();
-                    } else {
-                        if (typeof excludes === 'string') {
-                            excludes = [excludes];
-                        }
-                        rule.walkDecls(function (decl) {
-                            if (excludes.indexOf(decl.prop) !== -1) {
-                                decl.remove();
-                            }
-                        });
-                    }
+        if (!rulesToRemove) {
+            return;
+        }
+
+        css.walkAtRules(function (rule) {
+            var excludes = rulesToRemove[rule.name];
+            if (!excludes) {
+                return;
+            }
+
+            // One or more rules should be removed
+            if (excludes === '*') {
+                // Remove this entire rule
+                rule.remove();
+                return;
+            }
+
+            if (typeof excludes === 'string') {
+                excludes = [excludes];
+            }
+
+            rule.walkDecls(function (decl) {
+                if (excludes.indexOf(decl.prop) !== -1) {
+                    decl.remove();
                 }
             });
-        }
+        });
     };
 });
